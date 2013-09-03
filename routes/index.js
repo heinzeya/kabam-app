1bffe92bb286145e797978e45524a0edffe48265
T. Budiman <tbudiman@gmail.com>
2013-09-03 07:27:10 +0700

    feat(all): generated by kabam.js create (e283429754)

--- routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = exports = function (kabam) {
-  kabam.app.get('/', function (request, response) {
-    response.render('index', {userAgent: request.headers['user-agent'], title: 'Welcome!'})
-  });
-};
