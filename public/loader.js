c388298636a48022f300ed4834592c1f3c717efb
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-10-30 14:24:01 +0700

    chore(loader.js): remove unused dependencies
    Removed:
    - angular router

--- public/loader.js
+++ public/loader.js
@@ -17,7 +17,6 @@
 //= require fullcalendar/fullcalendar.js
 //= require angular-ui-calendar/src/calendar.js
 //= require restangular/dist/restangular.js
-//= require angular-route/angular-route.js
 //= require states/states.js
 //= require auth/main.js
 
